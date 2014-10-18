41e139a767e6aa1dbe2185cf736fa8587c3130c6
wangjohn <jwcitadel@gmail.com>
2014-10-17 21:46:04 -0700

    Adding tests for target vectors.

---
+++ philarios/philarios_test.go
@@ -0,0 +1,27 @@
+package philarios
+
+import (
+  "testing"
+)
+
+func TestForBeginningWordTargetVector(t *testing.T) {
+  fixtures := []struct {
+    paragraph string
+    beginningVector string
+  }{
+    {"My booty don't lie", "My"},
+    {"", ""},
+    {"Tabitha", "Tabitha"},
+    {"Nineteen. Ninety five.", "Nineteen"},
+    {"Chain-smokers.", "Chain-smokers"},
+    {"--bad things happen", "bad"},
+  }
+
+  for _, fixture := range fixtures {
+    resultingWord := lookForWord(fixture.paragraph, true)
+    if resultingWord != fixture.beginningVector {
+      t.Errorf("Did not find the expected vector. Expected '%s' but received '%s'",
+        fixture.beginningVector, fixture.resultingWord)
+    }
+  }
+}
