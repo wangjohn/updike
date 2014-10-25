2e7507327c5dcdee45dfc4d301fc78f07e64efc1
wangjohn <jwcitadel@gmail.com>
2014-10-24 21:57:07 -0700

    Documentation for splitwords

--- philarios/word_utils.go
+++ philarios/word_utils.go
@@ -5,6 +5,11 @@ import (
   "unicode"
 )
 
+/*
+SplitWords takes a string and returns the separate words that make up the
+string. In essence, it splits the string according to demarcating characters
+like punctuation and spaces.
+*/
 func SplitWords(sentence string) ([]string) {
   f := func(c rune) bool {
     return unicode.IsPunct(c) || unicode.IsSpace(c) || unicode.IsSymbol(c)
